50ece59a66abae9bf661e0531344a78e6687a2d3
Ashton Pate <arpate@amarillocollege.com>
2025-02-04 13:06:38 -0600

    use an handle input

--- classasignment 2 4 2025/Program.cs
+++ classasignment 2 4 2025/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace classasignment_2_4_2025
 {
+    
     internal class Program
     {
+         static double HandleDoubleInput(string aMessage)
+          {
+            double returnValue = double.MaxValue;
+              do
+            {
+                try
+                {
+                    Console.WriteLine(aMessage);
+
+                    returnValue = Convert.ToDouble(Console.ReadLine());//this method can trigger an exception when format number is input
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("invaild input please input a valid numebr");
+                }
+            } while (returnValue == double.MaxValue) ; //keep repeat when the varbil still have the initate value
+            return returnValue;
+        }
         static void Main(string[] args)
         {
             //quiz: calualte the total cost of a sale
@@ -23,36 +45,24 @@ namespace classasignment_2_4_2025
 
             double totalsale=double.MaxValue;//intiate value
 
-            //ask for the price
+              //ask for the price
 
-            //keep asking user to input a value until a vaild value is input
+              //keep asking user to input a value until a vaild value is input
 
-            do
-            {
-                try
-                {
-                    Console.WriteLine("how much is the price?");
+               price = HandleDoubleInput("what is the price");
 
-                    price = Convert.ToDouble(Console.ReadLine());//this method can trigger an exception when format number is input
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("invaild input");
-                }
-            } while (price == double.MaxValue); //keep repeat when the varbil still have the initate value
-         
-        
-            
 
-            //ask for promotion
-            Console.WriteLine("how much is the promotion(decmial)");
 
-            promotion = Convert.ToDouble(Console.ReadLine());
+
+             //ask for promotion
+
+
+              promotion = HandleDoubleInput(" what is the promotion in decmial");
 
             //ask for tax
-            Console.WriteLine("how much is the tax(decmail)");
 
-            tax = Convert.ToDouble(Console.ReadLine());
+
+              tax = HandleDoubleInput("what is the tax in decmial");
 
             //caluate total
             totalsale = price-(1-promotion)- (1-tax);
